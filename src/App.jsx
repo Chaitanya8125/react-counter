55ec3980de8ad0e1c818ce07781a0dbf3e729f93
Abhijith <24kb1a05e6@nbkrist.org>
2026-09-25 11:28:59 +0530

    React counter class component

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React, {Component } from "react";
+
+class Counter extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      count: 0
+    };
+  }
+  increment = () => {
+    this.setState({
+      count: this.state.count+1
+    });
+  };
+  decrement = () => {
+    this.setState({
+      count: this.state.count-1
+    });
+  };
+  reset =() => {
+    this.setState({
+      count: 0
+    });
+  };
+
+  render(){
+    return(
+      <div style={{ textAlign: "center", marginTop: "100px"}}>
+        <h1>React counter</h1>
+        <h2>Count: {this.state.count}</h2>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+        <button onClick={this.reset}>Reset</button>
+      </div>
+    );
+  }
+}
+export default Counter;
